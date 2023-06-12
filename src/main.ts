eee24b2317357f4425194ea8fd2d287d2d26ce7b
agustin-soluntech <agustin@soluntech.com>
2023-06-12 19:25:23 -0300

    products & users dto

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('barbershop API')
+    .setDescription('The barbershop API')
+    .addTag('users')
+    .addTag('products')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, document);
   await app.listen(3000);
 }
 bootstrap();
